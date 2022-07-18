cabb971227761acdc281dca56f8af77050bddee7
Will Downey <willd20@vt.edu>
2022-07-18 16:53:05 -0400

    Add route for toggling importance of a note

--- index.js
+++ index.js
@@ -19,6 +19,21 @@ const generateId = () => {
   return maxId + 1
 }
 
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+});
+
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
@@ -43,13 +58,13 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
 
 app.get('/api/notes/:id', (req, res, next) => {
   Note.findById(req.params.id)
