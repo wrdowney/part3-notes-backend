861d86e142d34276d2d7cf1d8ad3e78c5e9c9522
Will Downey <willd20@vt.edu>
2022-07-18 16:51:10 -0400

    Add error handling for malformed ids

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const app = express()
 const Note = require('./models/note');
 
 app.use(express.static('build'));
+app.use(express.json());
 
-app.use(express.json())
+
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
@@ -49,13 +50,31 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then(note => {
+      if(note) res.json(note);
+      else res.status(404).end() // note doesn't exist
+    })
+    .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
