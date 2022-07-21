fe8993a52d39906cc352ebcb8ff40e73fbedf74a
Will Downey <willd20@vt.edu>
2022-07-21 08:05:24 -0400

    Reorganize file structure

--- index.js
+++ index.js
@@ -1,91 +1,11 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const Note = require('./models/note');
+const app = require('./app'); // the actual Express application
+const http = require('http');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-app.use(express.static('build'));
-app.use(express.json());
+const server = http.createServer(app);
 
-
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
-});
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const { content, important } = request.body;
-
-  Note.findByIdAndUpdate(
-    request.params.id,
-    { content, important },
-    { new: true, runValidators: true, context: 'query' } // runs validation checks on updated data
-  )
-    .then(updatedNote => {
-      response.json(updatedNote);
-    })
-    .catch(error => next(error));
-});
-
-app.post('/api/notes', (request, response, next) => {
-  const body = request.body;
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' });
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  });
-
-  note.save().then(savedNote => {
-    response.json(savedNote);
-  })
-    .catch(error => next(error));
-});
-
-app.get('/api/notes', (req, res) => {
-  Note.find({}).then(notes => {
-    res.json(notes);
-  });
-});
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch(error => next(error));
-});
-
-app.get('/api/notes/:id', (req, res, next) => {
-  Note.findById(req.params.id)
-    .then(note => {
-      if(note) res.json(note);
-      else res.status(404).end(); // note doesn't exist
-    })
-    .catch(error => next(error));
-});
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
-  }
-
-  next(error);
-};
-
-// this has to be the last loaded middleware.
-app.use(errorHandler);
-
-// eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
 
